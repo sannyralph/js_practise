967e773d560722021aff5049a7dec988be493e32
sannyralph <sannylauren@gmail.com>
2022-10-01 01:10:46 +0200

    conditioner and loops practise

--- index.js
+++ index.js
@@ -1,5 +1,67 @@
-for (let i = 0; i <=15; i++){
-    console.log(i)
-}
+// for (let i = 0; i <=15; i++){
+//     console.log(i)
+// }
 
-let num = [1, 0, ]
+// let num = [1, 0, ]
+
+
+// let number = 20;
+
+// if(number > 0){
+//     number = "number is greater than 0"; 
+// } else {
+//     number = false;
+// }
+// console.log(number)
+
+// let name = "John";
+// let age = 17;
+// let highSchool = true; 
+
+
+// if (age >= 18 && highSchool == true){
+//     "pass";
+// }
+// else {
+//     "not qulified";
+// }
+  
+// let hour = 15; 
+// if ( hour >= 6 && hour <= 12){
+//     console.log("on time")
+// } else {
+//     console.log("too latee")
+// }
+
+// let name = "john";
+// let age = 17;
+// let highSchool = false; 
+
+// if (age >= 18 && highSchool === true) {
+//     console.log("qualified")
+// } else {
+//     console.log("try again when you are over 18 with an highschool")
+// }
+
+
+// for (i = 0; i <= 15; i++) {
+//     console.log(i)
+// };
+
+// let cars = ["banz", "bmw", "toyota", "jeep"]; 
+
+// for (i = 0; i < cars.length; i++){
+//     console.log(cars[i]);
+// }
+
+let data = {
+    name: "kind",
+    age: 47,
+    sex: "male",
+}
+console.log(data["name"])
+
+
+// for (let elem in data) {
+//     console.log(data["name"])
+// }
